18df73fa2fe8a9ddb757b1c7e651434df9b28ff4
hanhuibing <patrioters@163.com>
2019-02-15 11:49:42 +0800

    extends functions

--- myflink/src/main/java/com/cn/mine/myflink/HDFSDemo.java
+++ myflink/src/main/java/com/cn/mine/myflink/HDFSDemo.java
@@ -2,6 +2,7 @@ package com.cn.mine.myflink;
 
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.core.fs.FileSystem.WriteMode;
 
 public class HDFSDemo {
 	public static String HDFD_PATH = "hdfs://cdh2:8020/spark/can/2019/02/14/part-000151550073600000";
@@ -9,12 +10,15 @@ public class HDFSDemo {
 		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 		DataSet<String> data = env.readTextFile(HDFD_PATH);
 		
+		//写入本地文件
+		data.writeAsText("file:///D:\\flink\\test10", WriteMode.OVERWRITE).setParallelism(1);
+		
+		//控制台输出
 		try {
 			data.print();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 }
